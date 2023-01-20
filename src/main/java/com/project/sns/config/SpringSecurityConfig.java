07e7d99e4aa44fa2f408e4f12769e72a26dac0c3
Min Seop Kim <k_ms1998@naver.com>
2023-01-20 17:38:41 +0900

    Edit #3: 테스트 통과하도록 Spring Secuirty 수정

--- src/main/java/com/project/sns/config/SpringSecurityConfig.java
+++ src/main/java/com/project/sns/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.project.sns.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +12,7 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest()
                         .permitAll())
