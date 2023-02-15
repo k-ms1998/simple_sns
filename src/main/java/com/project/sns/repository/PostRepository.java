b2187d6f1f6dac39321e4d70679af63d656dd037
Min Seop Kim <k_ms1998@naver.com>
2023-02-15 17:28:14 +0900

    Update #5: 포스트 조회 로직 구현 + 스프링 시큐리티 ignoring() 경로 추가
    * 모든 포스트 조회 로직 구현
    * 회원 본인의 포스트만 조회하는 로직 구현
    * 회원에 따라서 포스트 가져오는 메서드를 리포지토리에 추가
    * 스프링 시큐리티에서 모든 포스트를 조회하는 path는 로그인 인증을 하지 않도록 수정

--- src/main/java/com/project/sns/repository/PostRepository.java
+++ src/main/java/com/project/sns/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.project.sns.repository;
 
 import com.project.sns.domain.PostEntity;
+import com.project.sns.domain.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    Page<PostEntity> findAllByUserEntity(Pageable pageable, UserEntity userEntity);
 }
